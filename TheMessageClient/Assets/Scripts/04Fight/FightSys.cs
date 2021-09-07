e0edc9c1dd50c733d2808eec1feb37660edb9f8f
Dun180 <74287755+Dun180@users.noreply.github.com>
2021-09-07 21:14:57 +0800

    before_rectification

--- TheMessageClient/Assets/Scripts/04Fight/FightSys.cs
+++ TheMessageClient/Assets/Scripts/04Fight/FightSys.cs
@@ -75,4 +75,6 @@ public class FightSys : MonoBehaviour
 
         infoWindow.RefreshWindow();
     }
+
+
 }
---
+++ TheMessageServer/PEProtocol/Card.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PEProtocol
+{
+    public class Card
+    {
+        public CardColor color;
+        public CardType type;
+        public CardFunction function;
+
+        public enum CardColor
+        {
+            None = 0,
+            Red = 1,
+            Blue = 2,
+            Black = 3,
+            RedBlack = 4,
+            BlueBlack = 5
+        }
+
+        public enum CardType
+        {
+            None = 0,
+            RestrictedMessage = 1,
+            NonstopMessage = 2,
+            TextMessage = 3
+        }
+
+        public enum CardFunction
+        {
+
+        }
+    }
+}
